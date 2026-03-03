6bc837621012a5dd2460236acbfee425eb98d726
João <moreirajoaopaulo951@gmail.com>
2026-03-03 14:46:12 -0300

    dependencias servlet instaladas e classe createAdoServlet criada

---
+++ src/main/java/br.com.ado.servlet/CreateAdoServlet.java
@@ -0,0 +1,11 @@
+package br.com.ado.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+public class CreateAdoServlet {
+    @WebServlet("./create-ado")
+    public  class  CreateAdoSerlet extends HttpServlet {
+
+    }
+}
