456181bcb8da2f92671a47877faf36f3681c79eb
João <moreirajoaopaulo951@gmail.com>
2026-03-04 12:22:36 -0300

    adicionado json no doPost

--- src/main/java/br.com.ado.servlet/CreateAdoServlet.java
+++ src/main/java/br.com.ado.servlet/CreateAdoServlet.java
@@ -1,11 +1,28 @@
 package br.com.ado.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.rmi.server.ServerCloneException;
 
-public class CreateAdoServlet {
-    @WebServlet("./create-ado")
-    public  class  CreateAdoSerlet extends HttpServlet {
+    @WebServlet("/create-ado")
+    public  class  CreateAdoServlet extends HttpServlet {
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+            String adoItem = request.getParameter("ado-item");
+            System.out.println(adoItem);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            String json = "{\"item\":\"" + adoItem + "\"}";
+            response.getWriter().write(json);
+
+
+        }
     }
-}
